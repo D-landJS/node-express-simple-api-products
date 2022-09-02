e7f4457a8b0cfa4b7ade90246a63c76fead41fd8
D-LAND\Leon <d-landarrese80@hotmail.com>
2022-09-02 04:29:29 -0500

    api products finished

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const conectDB = async () => {
+	const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.dg4wm.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 	try {
-		await mongoose.connect(process.env.DB_MONGO);
+		await mongoose.connect(uri);
 
 		console.log('DB conectado');
 	} catch (error) {
